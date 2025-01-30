7442b6ac0af4b748b608ae45c6d66b386ec2c9c1
mbaisotti99 <matteo.baisotti@gmail.com>
2025-01-30 12:47:10 +0100

    feat: bugfix

--- components/Details.jsx
+++ components/Details.jsx
@@ -22,8 +22,6 @@ const Details = () =>{
         axios.get(`http://localhost:3000/movies/${id}`)
         .then((resp) =>{
             setMovie(resp.data.data)
-        }).finally(() =>{
-            console.log(movie);
         })
     }
     
@@ -39,7 +37,7 @@ const Details = () =>{
         let newFormData = {...review}
         newFormData[name] = value 
         setReview(newFormData)
-        console.log(review);
+        // console.log(review);
         
     }
 
@@ -47,7 +45,7 @@ const Details = () =>{
     const handleSubmit = () =>{
         axios.post(`http://localhost:3000/movies/${id}/review`, review)
         .then((resp) =>{
-            console.log(resp);
+            // console.log(resp);
             getMovie()
         })
     }
@@ -58,20 +56,20 @@ const Details = () =>{
 
     // const numbers = Array.from({ length: 5 }, (_, i) => i + 1);
     const stars = Array.from({ length: 5 }, (_, i) => (
-        <p key={i+1}>{"⭐".repeat(i + 1)}</p>
+        "⭐".repeat(i + 1)
       ));
 
       
 
     return(
         <div className="container text-center">
-            <h1>{movie.title}</h1>
+            <h1 className="my-5" >{movie.title}</h1>
             <img width={500} src={`http://localhost:3000/${movie.images}`} alt={`Immagine di ${movie.title}`} />
             <p className="my-5">{movie.abstract}</p>
             <h2>Reviews</h2>
-                {(movie.reviews) && movie.reviews.map((curReview) =>{
+                {(movie.reviews) && movie.reviews.map((curReview, i) =>{
                     return(
-                        <ul className="list-unstyled my-5">
+                        <ul className="list-unstyled my-5" key={i}>
                             <li>Reviewer: {curReview.name}</li>
                             <li>Vote: {printStars(curReview.vote)}</li>
                             <li>Description: {curReview.text}</li>
@@ -85,14 +83,14 @@ const Details = () =>{
                 <input className="form-control mb-5" type="text" id="user" name="name" onChange={handleChange}/>
                 <h2>Vote:</h2> 
                 <select className="form-control text-center fs-2 mb-5" id="vote" name="vote" onChange={handleChange}>
-                    {stars.map((curNum) =>
-                    <option value={curNum.key}>{curNum}</option>
+                    {stars.map((curNum, i) =>
+                    <option key={i} value={i + 1}>{curNum}</option>
                     )}
                 </select>
                 <h2 className="mb-3">Text:</h2>
                 <textarea className="form-control mb-5" type="text" id="text" name="text" onChange={handleChange}/>
                 <br />
-                <button type="submit" className="btn btn-primary">Aggiungi Recensione</button>
+                <button type="submit" className="btn btn-primary mb-5">Aggiungi Recensione</button>
             </form>
         </div>
     )
